f76dabd6f96f449a5a9a9e1a2f1d67f295b4de5a
Raymond Umbas <raymond.umbas@gmail.com>
2026-05-30 22:37:47 -0700

    insertData wrapper function

--- guess-what/src/util/functions.js
+++ guess-what/src/util/functions.js
@@ -1,5 +1,17 @@
-import supabase from './config/supabase-config';
+import supabase from '../config/supabase-config';
 
+
+/**
+ * Wrapper for getting data from supabase
+ *
+ * @function getData
+ * @param {string} from - DB Table name
+ * @param {string} select - Desired table field name
+ * @param {object} eq - "field" = table field name, "value" = target value
+ * @returns {Promise<object>} Resolves to the retrieved DB data
+ * @throws {Error} Throws an error if supabase fetch fails
+ * 
+ */
 export async function getData(from, select, eq) {
      
     const { data, error } = await supabase
@@ -9,15 +21,45 @@ export async function getData(from, select, eq) {
                 
                 if(error){
 
-                   console.log(error);
                    throw error;
 
                 }
 
                 else{
 
+                    console.log(`Fetched data from ${from}`,data);
                     return data;
 
                 }
+
+}
+
+
+
+/**
+ * Wrapper for inserting into supabase
+ *
+ * @function insertData
+ * @param {string} from - DB Table name
+ * @param {object} dataObject - Key = table fields, Value = field values
+ * @returns {Promise<object>} Resolves to the retrieved DB data
+ * @throws {Error} Throws an error if supabase fetch fails
+ * 
+ */
+export async function insertData(from,dataObject){
+
+            const { data, error } = await supabase.from(from).insert(dataObject)
+
+        if(error){
+
+          throw error;
+
+        }
+
+        else{
+
+          console.log(`Inserted data to ${from}`,data);
+
+        }
 
 }
